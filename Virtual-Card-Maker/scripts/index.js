d026bff20c3e34c1ded0e237127e2e6fc0f1cc0d
Siva Prasanna <shivachatti66@gmail.com>
2025-02-15 09:46:23 +0530

    Add files via upload

---
+++ Virtual-Card-Maker/scripts/index.js
@@ -0,0 +1,20 @@
+const btn1 = document.querySelector("#login");
+const btn2 = document.querySelector("#signup");
+const btn3 = document.querySelector("#about");
+const btn4 = document.querySelector("#templates");
+const btn5 = document.querySelector("#image");
+btn1.addEventListener("click", () => {
+    window.location.href = "https://8n1h4k94-5501.inc1.devtunnels.ms/login.html";
+});
+btn2.addEventListener("click", () => {
+    window.location.href = "https://8n1h4k94-5501.inc1.devtunnels.ms/sign-up.html";
+})
+btn3.addEventListener("click", () => {
+    window.location.href = "https://8n1h4k94-5501.inc1.devtunnels.ms/about.html";
+})
+btn4.addEventListener("click", () => {
+    window.location.href = "https://8n1h4k94-5501.inc1.devtunnels.ms/card.html";
+})
+// btn5.addEventListener("click", () => {
+//     window.location.href = "https://8n1h4k94-5501.inc1.devtunnels.ms/card.html"
+// })
